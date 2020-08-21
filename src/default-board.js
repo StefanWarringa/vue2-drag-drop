cefbe07d503fa7a79963120c09739ff873f31555
Stefan Warringa <swarringa@intreq.nl>
2020-08-21 14:04:28 +0200

    Default board simple todo-doing-done without tasks

--- src/default-board.js
+++ src/default-board.js
@@ -1,52 +1,18 @@
-import { uuid } from './utils'
-
 export default {
+  id: 0,
   name: 'myfirstboard',
   columns: [
     {
-      name: 'todo',
-      tasks: [
-        {
-          description: 'This is the first task to complete',
-          name: 'first task',
-          id: uuid(),
-          userAssigned: null
-        },
-        {
-          description: 'This is to be done after the first task',
-          name: 'second task',
-          id: uuid(),
-          userAssigned: null
-        },
-        {
-          description: '',
-          name: 'and third',
-          id: uuid(),
-          userAssigned: null
-        }
-      ]
+      name: 'To-Do',
+      tasks: []
     },
     {
-      name: 'in-progress',
-      tasks: [
-        {
-          description: '',
-          name: 'first task',
-          id: uuid(),
-          userAssigned: null
-        }
-      ]
+      name: 'Doing',
+      tasks: []
     },
     {
-      name: 'done',
-      tasks: [
-        {
-          description: '',
-          name: 'first task',
-          id: uuid(),
-          userAssigned: null
-        }
-      ]
+      name: 'Done',
+      tasks: []
     }
   ]
 }
